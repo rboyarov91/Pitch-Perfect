6822c09aa819502295a72a8d98d07ad068e3b2ed
Roman <rboyarov91@gmail.com>
2015-10-23 11:35:20 -0400

    add record button to UI

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        //TODO display text that it's recording
+        //TODO actually start recording
+    }
 
 }
 
